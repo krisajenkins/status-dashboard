b327b118d4c21abd275d338435570977a218dba6
Kris Jenkins <krisajenkins@gmail.com>
2018-07-12 16:22:10 +0100

    Healthcheck now gets real data.
    We need to be a bit smarter about sharing data between threads though,
    and the backend URL is hardcoded. Still, very close.

--- server/src/Healthcheck.hs
+++ server/src/Healthcheck.hs
@@ -1,9 +1,13 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
 
 module Healthcheck where
 
+import Control.Lens (makeLenses, makePrisms)
 import Data.Aeson
   ( FromJSON
   , ToJSON
@@ -21,9 +25,24 @@ import Data.Aeson
 import Data.Aeson.Types (Parser)
 import Data.List ()
 import Data.Monoid ((<>))
+import Data.Proxy (Proxy(Proxy))
 import Data.Text (Text)
 import qualified Data.Vector as Vector
 import GHC.Generics (Generic)
+import Network.HTTP.Client (Manager)
+import Servant ((:>), Get, JSON)
+import Servant ()
+import Servant.Client
+  ( BaseUrl
+  , ClientEnv
+  , ClientM
+  , Scheme
+  , ServantError
+  , client
+  , manager
+  , mkClientEnv
+  , runClientM
+  )
 
 newtype HealthcheckResponse = HealthcheckResponse
   { _checks :: [Check]
@@ -53,3 +72,16 @@ instance FromJSON Check where
 newtype ErrorMessage =
   ErrorMessage Text
   deriving (Show, Eq, Generic, FromJSON)
+
+makeLenses ''HealthcheckResponse
+
+makePrisms ''Check
+
+------------------------------------------------------------
+type MantisAPI = "healthcheck" :> Get '[ JSON] HealthcheckResponse
+
+getHealthcheck :: ClientM HealthcheckResponse
+getHealthcheck = client (Proxy :: Proxy MantisAPI)
+
+run :: Manager -> BaseUrl -> ClientM a -> IO (Either ServantError a)
+run manager base action = runClientM action $ mkClientEnv manager base
--- server/src/Webserver.hs
+++ server/src/Webserver.hs
@@ -6,16 +6,24 @@ module Webserver
   ) where
 
 import Control.Concurrent (threadDelay)
+import Control.Concurrent.MVar (MVar)
+import Control.Lens ((^.), contains, has, traversed)
+import Control.Monad (forever)
 import Control.Monad.Except (throwError)
 import Control.Monad.IO.Class (MonadIO, liftIO)
-import Control.Monad.Logger (runStderrLoggingT)
+import Control.Monad.Logger (logDebug, logDebugN, runStderrLoggingT)
 import Data.Aeson (Value(Number), encode, object)
 import Data.Default.Class (def)
 import Data.Foldable (for_)
 import Data.Monoid ((<>))
 import Data.Proxy (Proxy(Proxy))
 import Data.Text (Text, pack)
+import qualified Data.Text as Text
 import Development.GitRev (gitHash)
+import Healthcheck (HealthcheckResponse, _Error, checks, getHealthcheck)
+import qualified Healthcheck
+import Network.HTTP.Client (Manager, newManager)
+import Network.HTTP.Client.TLS (tlsManagerSettings)
 import Network.Wai (Application, Middleware)
 import Network.Wai.Middleware.Cors (simpleCors)
 import Network.Wai.Middleware.Gzip (gzip)
@@ -30,12 +38,22 @@ import Servant
   , serveDirectoryFileServer
   )
 import Servant.API.WebSocket (WebSocket)
+import Servant.Client (BaseUrl(BaseUrl), Scheme(Https), ServantError)
 import Servant.Server (Handler, Server)
 import Webserver.API (Web)
-import Webserver.Types (OverallStatus(Bad, Good))
+import Webserver.Types (OverallStatus(Bad, Good, Unknown))
 
 api :: Handler Text
 api = pure "Hello"
+
+summarise :: Either ServantError HealthcheckResponse -> OverallStatus
+summarise (Left _) = Unknown
+summarise (Right response) =
+  if has (checks . traversed . _Error) response
+    then Bad
+    else Good
+
+healthcheckUrl = BaseUrl Https "staging.iele.dev-mantis.iohkdev.io" 8546 ""
 
 websocket :: Server WebSocket
 websocket = serveWebsocket
@@ -43,14 +61,12 @@ websocket = serveWebsocket
     serveWebsocket :: MonadIO m => Connection -> m ()
     serveWebsocket connection =
       liftIO $ do
+        manager <- newManager tlsManagerSettings
         forkPingThread connection 10
-        for_ [1 .. 10] $ \i -> do
-          sendTextData connection $
-            encode $
-            if i < 8
-              then Good
-              else Bad
-          threadDelay 1000000
+        forever $ do
+          response <- Healthcheck.run manager healthcheckUrl getHealthcheck
+          sendTextData connection $ encode $ summarise response
+          threadDelay 5000000
 
 version :: Applicative m => m Text
 version = pure $(gitHash)
